afdd2a9155ba87a59ced57ee44129dc5f06d5745
Akseli Arvaja <akseli.arvaja@student.tut.fi>
2017-06-16 00:36:23 +0300

    jeee

--- ledshow.py
+++ ledshow.py
@@ -1,35 +1,8 @@
-buttonslist = []
 
 from logipy import logi_led
 import time
-
-def flash_rgb(code):
-    r = 100
-    g = 0
-    b = 0
-    for i in range (20):
-        logi_led.logi_led_set_lighting_for_key_with_hid_code(code,100,100,100)
-        logi_led.logi_led_set_lighting_for_key_with_hid_code(code,r,g,b)
-        b +=5
-        r -=5
-        time.sleep(0.00001)
-    for i in range (20):
-        logi_led.logi_led_set_lighting_for_key_with_hid_code(code,100,100,100)
-
-        logi_led.logi_led_set_lighting_for_key_with_hid_code(code,r,g,b)
-        g +=5
-        b -=5
-        time.sleep(0.00001)
-
-    for i in range (20):
-        logi_led.logi_led_set_lighting_for_key_with_hid_code(code,100,100,100)
-
-        logi_led.logi_led_set_lighting_for_key_with_hid_code(code,r,g,b)
-        r +=5
-        g -=5
-        time.sleep(0.00001)
-
-
+import random
+from multiprocessing import Process
 NAME = 0
 CODE = 1
 NEAR = 2
@@ -38,25 +11,56 @@ DICT_CODE = 0
 DICT_NEAR = 1
 DICT_MID = 2
 
-logi_led.logi_led_init()
-file = open("näppäimet.csv","r")
-buttonlines = file.readlines()
-buttonsdict= {}
+
+def flash_rgb():
+    r = 100
+    g = 0
+    b = 0
+    code =  random.randint(4,100)
+    for i in range (20):
+        logi_led.logi_led_set_lighting_for_key_with_hid_code(code,100,100,100)
+        logi_led.logi_led_set_lighting_for_key_with_hid_code(code,r,g,b)
+        b +=5
+        r -=5
+        time.sleep(0.01)
+    for i in range (20):
+        logi_led.logi_led_set_lighting_for_key_with_hid_code(code,100,100,100)
+
+        logi_led.logi_led_set_lighting_for_key_with_hid_code(code,r,g,b)
+        g +=5
+        b -=5
+        time.sleep(0.01)
+
+    for i in range (20):
+        logi_led.logi_led_set_lighting_for_key_with_hid_code(code,100,100,100)
+
+        logi_led.logi_led_set_lighting_for_key_with_hid_code(code,r,g,b)
+        r +=5
+        g -=5
+        time.sleep(0.01)
 
 
-for line in buttonlines:
-    data = line.replace("\n","").split(";")
-    number, button, close_buttons, mid_buttons = int(data[0]), data[1], data[2].split(","), data[3].split(",")
-    buttonslist.append([button,number,close_buttons,mid_buttons])
-    buttonsdict[button] = [number,close_buttons,mid_buttons]
+if __name__ == "__main__":
 
-time.sleep(1.5)
-for led in buttonslist:
-    logi_led.logi_led_set_lighting(100, 0, 0)
-    logi_led.logi_led_set_lighting_for_key_with_hid_code(int(led[CODE]), 0, 100, 0)
-    print(led[NAME])
-    for lamp in led[NEAR]:
-        flash_rgb(buttonsdict[lamp][DICT_CODE])
-    time.sleep(0.01)
-time.sleep(5)
+    logi_led.logi_led_init()
+    file = open("näppäimet.csv","r")
+    buttonlines = file.readlines()
+    buttonsdict= {}
+    buttonslist = []
+
+    for line in buttonlines:
+        data = line.replace("\n","").split(";")
+        number, button, close_buttons, mid_buttons = int(data[0]), data[1], data[2].split(","), data[3].split(",")
+        buttonslist.append([button,number,close_buttons,mid_buttons])
+        buttonsdict[button] = [number,close_buttons,mid_buttons]
+
+    time.sleep(1.5)
+    for led in buttonslist:
+        logi_led.logi_led_set_lighting(100, 0, 0)
+        logi_led.logi_led_set_lighting_for_key_with_hid_code(int(led[CODE]), 0, 100, 0)
+        print(led[NAME])
+        for lamp in led[NEAR]:
+            Process(target=flash_rgb).start()
+        time.sleep(0.01)
+    time.sleep(5)
 
--- testi.py
+++ testi.py
@@ -1,12 +1,54 @@
+buttonslist = []
+
 from logipy import logi_led
 import time
+import random
+from multiprocessing import Process
 
-logi_led.logi_led_init()
-time.sleep(1)
-logi_led.logi_led_set_lighting(100,0,0)
-for i in range (1,350):
-    logi_led.logi_led_set_lighting_for_key_with_hid_code(i,0,100,0)
-    print(i)
-    time.sleep(0.1)
 
-time.sleep(12)
+def flash_rgb():
+    r = 100
+    g = 0
+    b = 0
+    code =  random.randint(4,100)
+    for i in range (20):
+        logi_led.logi_led_set_lighting_for_key_with_hid_code(code,100,100,100)
+        logi_led.logi_led_set_lighting_for_key_with_hid_code(code,r,g,b)
+        b +=5
+        r -=5
+        time.sleep(0.01)
+    for i in range (20):
+        logi_led.logi_led_set_lighting_for_key_with_hid_code(code,100,100,100)
+
+        logi_led.logi_led_set_lighting_for_key_with_hid_code(code,r,g,b)
+        g +=5
+        b -=5
+        time.sleep(0.01)
+
+    for i in range (20):
+        logi_led.logi_led_set_lighting_for_key_with_hid_code(code,100,100,100)
+
+        logi_led.logi_led_set_lighting_for_key_with_hid_code(code,r,g,b)
+        r +=5
+        g -=5
+        time.sleep(0.01)
+
+
+if __name__ == "__main__":
+    logi_led.logi_led_init()
+    file = open("näppäimet.csv","r")
+    buttonlines = file.readlines()
+    buttonsdict= {}
+    buttonslist = []
+
+    for line in buttonlines:
+        data = line.replace("\n","").split(";")
+        number, button, close_buttons, mid_buttons = int(data[0]), data[1], data[2].split(","), data[3].split(",")
+        buttonslist.append([button,number,close_buttons,mid_buttons])
+        buttonsdict[button] = [number,close_buttons,mid_buttons]
+
+    logi_led.logi_led_init()
+    time.sleep(1)
+    while True:
+        logi_led.logi_led_set_lighting_for_key_with_hid_code(buttonslist[random.randint(0,len(buttonslist))][1], random.randint(0,100), random.randint(0,100), random.randint(0,100))
+        time.sleep(0.002)
